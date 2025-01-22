808b39aa71a525e88e34db7d88324cb9f209baf5
0xALUCARD <0xALUCARD@protonmail.com>
2025-01-22 17:59:02 +0100

    latest brewboo

--- src/BrewBoo.sol
+++ src/BrewBoo.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 // P1 - P3: OK
-pragma solidity 0.8.13;
+pragma solidity 0.8.28;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -69,13 +69,13 @@ contract BrewBooV3 is Ownable, ReentrancyGuard {
     event LogSlippageOverrode(address _adr);
 
     constructor(
-        address _factory,
-        address _xboo,
-        address _boo,
-        address _wftm,
-        address route1,
-        address route2
     ) {
+        address _factory = 0xEE4bC42157cf65291Ba2FE839AE127e3Cc76f741;
+        address _xboo = 0xa95eA1cfaBcCf0E9eb94b646CeFe9eD71ff5D605;
+        address _boo = 0x7A0C53F7eb34C5BC8B01691723669adA9D6CB384;
+        address _wftm = 0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38;
+        address route1 = 0x29219dd400f2Bf60E5a23d13Be72B486D4038894;
+        address route2 = 0x50c42dEAcD8Fc9773493ED674b675bE577f2634b;
         factory = IUniswapV2Factory(_factory);
         xboo = _xboo;
         boo = _boo;
